f572926e203cfadacccb14ba193a93c70598aa93
Priyanka-1920 <priyankarevri@outlook.com>
2025-11-22 20:26:00 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,5 @@
-from app import app
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-def test_index():
-    client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
+from app import app
